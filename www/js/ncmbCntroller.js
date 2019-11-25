22ce61ee2359fa5a66067798b4dfde56af415e34
k18rs074 <k18rs074@st.kyusan-u.ac.jp>
2019-11-25 13:56:15 +0900

    ncmbControllerの初期化

--- www/js/ncmbCntroller.js
+++ www/js/ncmbCntroller.js
@@ -3,4 +3,17 @@
 // ncmbController.js
 
 var ncmbController = {
+  APPLICATION_KEY:appKey,
+  CLIENT_KEY:clientKey,
+
+  ncmb:null,
+  currentUser:null,
+  screenSize:null,
+
+  init:function(screenSize) {
+        var self = this;
+        self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
+        self.screenSize = screenSize;
+    }
+
 }
